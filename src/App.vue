8faf5e7cccb1fe7ad8b0f232c046deb2fb7e6ab8
tmaclucien <huili50@iflytek.com>
2023-07-04 14:24:24 +0800

    规范源文件

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,8 @@
 <!-- @format -->
 
 <script setup lang="ts">
-import HelloWorld from "./components/HelloWorld.vue" // prettier-error: doubleQuoate -> singleQuate
+import HelloWorld from './components/HelloWorld.vue'
 
-const a = (e: any) => {
-  console.log(e); // prettier-error: semi(false) && eslint-error: no-console
-} // eslint-warning : no-unused-vars
-
-let b = 1; // eslint-error: prefer const && eslint-warning: no-unused-vars
 </script>
 
 <template>
@@ -15,20 +10,18 @@ let b = 1; // eslint-error: prefer const && eslint-warning: no-unused-vars
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
-    <!-- prettier-error: tabWidth -->
-<a href="https://vuejs.org/" target="_blank">
-<img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-</a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
   </div>
   <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-/* <!-- prettier-error: tabWidth --> */
 .logo {
-padding: 1.5em;
-will-change: filter;
-transition: filter 300ms;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
 }
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
